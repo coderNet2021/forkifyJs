71c1c6399a4334b5ba566cffd491b7647074ade2
coderNet2021 <cnss.saida@gmail.com>
2021-07-26 13:19:17 +0300

    tut293-mis

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './modele.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -106,7 +107,10 @@ const controlSearchResults = async function () {
     //all results
     //resultsView.render(model.state.search.results);
     //some results : 
-    resultsView.render(model.getSearchResultsPage());
+    resultsView.render(model.getSearchResultsPage(6));
+    
+    //4 ) render iniial pagination buttons
+    paginationView.render(model.state.search)
   } catch (error) {
     console.error(error);
     recipeView.renderError(); //`error : ${error} !!! OMG`
