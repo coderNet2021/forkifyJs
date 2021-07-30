e245df59f6a147a09f8de73999f79ce9e92caf4b
Lilyane Fawaz <cnss.saida@gmail.com>
2021-07-30 20:43:36 +0300

    final commit forkify

--- src/js/modele.js
+++ src/js/modele.js
@@ -13,12 +13,12 @@ export const state = {
     page: 1,
     resultsPerPage: RES_PER_PAGE,
   },
-  bookmarks:[],//recipe that are bookmarked
+  bookmarks: [], //recipe that are bookmarked
 };
 
-const createRecipeObject = function(data){
+const createRecipeObject = function (data) {
   const { recipe } = data.data;
-  return   {
+  return {
     id: recipe.id,
     title: recipe.title,
     publisher: recipe.publisher,
@@ -27,21 +27,21 @@ const createRecipeObject = function(data){
     servings: recipe.servings,
     cookingTime: recipe.cooking_time,
     ingredients: recipe.ingredients,
-    ...(recipe.key &&{key:recipe.key}),
+    ...(recipe.key && { key: recipe.key }),
   };
-}
+};
 
 export const loadRecipe = async function (id) {
   try {
     const data = await AJAX(`${API_URL}${id}?key=${API_KEY}`);
     //
     //let recipe = data.data.recipe;
-   state.recipe=createRecipeObject(data);
-   
-    
-    if(state.bookmarks.some(bookmark=> bookmark.id===id))//some means here any
-      state.recipe.bookmarked=true;
-      else state.recipe.bookmarked=false;
+    state.recipe = createRecipeObject(data);
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      //some means here any
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
     //console.log(state.recipe);
   } catch (error) {
     //temperory error handling
@@ -64,7 +64,7 @@ export const loadSearchResults = async function (query) {
         title: rec.title,
         publisher: rec.publisher,
         image: rec.image_url,
-        ...(rec.key &&{key:rec.key}),
+        ...(rec.key && { key: rec.key }),
       };
     });
     state.search.page = 1;
@@ -93,94 +93,88 @@ export const updateServings = function (newServings) {
   state.recipe.servings = newServings;
 };
 
-
-const persistBookmarks  = function (){
-  localStorage.setItem('bookmarks',JSON.stringify(state.bookmarks));
+const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
 };
-export const addBookmark = function(recipe){
+export const addBookmark = function (recipe) {
   //add bookmark
   state.bookmarks.push(recipe);
 
   //mark the current recipe as bookmarked
   //set a new property to the recipe object
-  if(recipe.id===state.recipe.id) state.recipe.bookmarked=true ;
-  
-  persistBookmarks();
-}
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
 
-export const deleteBookmark=function(id){
+  persistBookmarks();
+};
+
+export const deleteBookmark = function (id) {
   //delete Bookmark
-  const index = state.bookmarks.findIndex(bookmark=>bookmark.id===id)
-  state.bookmarks.splice(index,1);
-  
+  const index = state.bookmarks.findIndex(bookmark => bookmark.id === id);
+  state.bookmarks.splice(index, 1);
+
   //mark the current recipe as NOT  bookmarked
-  if(id===state.recipe.id) state.recipe.bookmarked=false ;
-  
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
+
   persistBookmarks();
 };
 
 // const init=function(){
 //  const storage =  localStorage.getItem('bookmarks');
- 
+
 //  if(storage) state.bookmarks=JSON.parse(storage);
 // };
 
 const init = function () {
   console.log('test');
-   const storage =  localStorage.getItem('bookmarks');
- 
- if(storage) state.bookmarks=JSON.parse(storage);
-}
+  const storage = localStorage.getItem('bookmarks');
+
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
 init();
 
 // console.log(state.bookmarks);
 
-const clearBookmarks= function(){
+const clearBookmarks = function () {
   localStorage.clear('bookmarks');
-}
+};
 // clearBookmarks();
 
-export const uploadRecipe= async function(newRecipe){
+export const uploadRecipe = async function (newRecipe) {
   try {
     //console.log(Object.entries(newRecipe));
-    const ingredients= Object.entries(newRecipe).filter(entry => entry[0].startsWith('ingredient') && entry[1] !=='')
-    .map(ing =>
-      {
-        const ingArr=ing[1].replaceAll(' ','')
-        .split(',');
-        if(ingArr.length !==3) throw new Error('Wrong ingredient format, please use the correct format');
-        
-        const [quantity,unit, description] =ingArr;
-       
-       return { quantity: quantity? +quantity:null ,unit, description};
-        
-    });
-    const recipe={
+    const ingredients = Object.entries(newRecipe)
+      .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
+      .map(ing => {
+        const ingArr = ing[1].split(',').map(el => el.trim());
+        //const ingArr=ing[1].replaceAll(' ','').split(',');
+        if (ingArr.length !== 3)
+          throw new Error(
+            'Wrong ingredient format, please use the correct format'
+          );
+
+        const [quantity, unit, description] = ingArr;
+
+        return { quantity: quantity ? +quantity : null, unit, description };
+      });
+    const recipe = {
       title: newRecipe.title,
-     publisher: newRecipe.publisher,
-     source_url : newRecipe.sourceUrl,
-     image_url: newRecipe.image,
-     servings: +newRecipe.servings,
-     cooking_time: +newRecipe.cookingTime,
-     ingredients
-   };
-   console.log('-----recipe ---- controller ---');
+      publisher: newRecipe.publisher,
+      source_url: newRecipe.sourceUrl,
+      image_url: newRecipe.image,
+      servings: +newRecipe.servings,
+      cooking_time: +newRecipe.cookingTime,
+      ingredients,
+    };
+    console.log('-----recipe ---- controller ---');
     console.log(recipe);
-    
-    const data = await  AJAX(`${API_URL}?key=${API_KEY}`,recipe);
+
+    const data = await AJAX(`${API_URL}?key=${API_KEY}`, recipe);
 
     console.log(data);
-    state.recipe= createRecipeObject(data);
-    
+    state.recipe = createRecipeObject(data);
+
     addBookmark(state.recipe);
-    
   } catch (error) {
     throw error;
-    
   }
-  
- 
-}
-
-
-  
+};
